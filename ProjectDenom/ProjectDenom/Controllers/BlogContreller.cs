9879ac5952f16f0128ab91d38ad5ae8cfd33f80d
AmilMammadli <amilanimasiya01@gmail.com>
2023-01-13 04:20:57 -0800

    f

---
+++ ProjectDenom/ProjectDenom/Controllers/BlogContreller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectDenom.Controllers
+{
+    public class BlogContreller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjectDenom/ProjectDenom/Controllers/MarriageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectDenom.Controllers
+{
+    public class MarriageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
